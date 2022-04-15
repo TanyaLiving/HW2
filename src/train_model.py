1a75798d35a4f0f6ea4c186257aeeda011268c62
Tatiana <77466010+TanyaLiving@users.noreply.github.com>
2022-04-15 16:27:02 +0300

    with open pickle correction

--- src/train_model.py
+++ src/train_model.py
@@ -32,7 +32,7 @@ def model_result(model, x_train, y_train, x_test, y_test):
     )
 
     with open("model_name.pickle", "wb") as test_model:
-        pickle.dump({'model': model}, test_model)
+        pickle.dump(model, test_model)
 
 def model_result_2(y_test, y_pred):
     """Return confusion matrix"""
