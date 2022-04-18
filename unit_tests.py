eb2fdce1788c773c8921a7c8392e2d67feb8d110
Tatiana <77466010+TanyaLiving@users.noreply.github.com>
2022-04-18 09:51:19 +0300

    snake_case naming style

--- unit_tests.py
+++ unit_tests.py
@@ -6,32 +6,32 @@ from src import Preparation, train_model
 class preparation_test(unittest.TestCase):
     def test_shape(self):
         self.assertEqual(
-            Preparation.train_X_transform.shape[1],
-            Preparation.test_X_transform.shape[1],
+            Preparation.train_x_transform.shape[1],
+            Preparation.test_x_transform.shape[1],
         )
         self.assertEqual(
-            Preparation.train_X_transform.shape[1],
-            Preparation.train_X_transform.shape[1],
+            Preparation.train_x_transform.shape[1],
+            Preparation.train_x_transform.shape[1],
         )
 
     def test_shape_equal(self):
         self.assertEqual(
-            Preparation.test_X_transform.shape[0], Preparation.test_y_transform.shape[0]
+            Preparation.test_x_transform.shape[0], Preparation.test_y_transform.shape[0]
         )
         self.assertEqual(
-            Preparation.train_X_transform.shape[0],
+            Preparation.train_x_transform.shape[0],
             Preparation.train_y_transform.shape[0],
         )
 
     def test_nan(self):
-        self.assertIsNotNone(Preparation.test_X_transform.isna().sum() == 0)
+        self.assertIsNotNone(Preparation.test_x_transform.isna().sum() == 0)
         self.assertIsNotNone(Preparation.test_y_transform.isna().sum() == 0)
-        self.assertIsNotNone(Preparation.train_X_transform.isna().sum() == 0)
+        self.assertIsNotNone(Preparation.train_x_transform.isna().sum() == 0)
         self.assertIsNotNone(Preparation.train_y_transform.isna().sum() == 0)
 
     def test_type(self):
         self.assertTrue(
-            Preparation.test_X_transform.select_dtypes(include="object").shape[1] == 0
+            Preparation.test_x_transform.select_dtypes(include="object").shape[1] == 0
         )
 
     def check_score_nan(self):
