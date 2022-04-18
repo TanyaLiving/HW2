a7df1ddb4c3775101da2168f24cd4526ef5363b6
Tatiana <77466010+TanyaLiving@users.noreply.github.com>
2022-04-18 10:43:43 +0300

    add docstring

--- unit_tests.py
+++ unit_tests.py
@@ -4,7 +4,9 @@ from src import Preparation, train_model
 
 
 class preparation_test(unittest.TestCase):
+    """unit tests for Preparation.py, train_model.py"""
     def test_shape(self):
+        """size testing (columns)"""
         self.assertEqual(
             Preparation.train_x_transform.shape[1],
             Preparation.test_x_transform.shape[1],
@@ -15,6 +17,7 @@ class preparation_test(unittest.TestCase):
         )
 
     def test_shape_equal(self):
+        """size testing (rows)"""
         self.assertEqual(
             Preparation.test_x_transform.shape[0], Preparation.test_y_transform.shape[0]
         )
@@ -24,17 +27,20 @@ class preparation_test(unittest.TestCase):
         )
 
     def test_nan(self):
+        """isna checking"""
         self.assertIsNotNone(Preparation.test_x_transform.isna().sum() == 0)
         self.assertIsNotNone(Preparation.test_y_transform.isna().sum() == 0)
         self.assertIsNotNone(Preparation.train_x_transform.isna().sum() == 0)
         self.assertIsNotNone(Preparation.train_y_transform.isna().sum() == 0)
 
     def test_type(self):
+        """type compliance"""
         self.assertTrue(
             Preparation.test_x_transform.select_dtypes(include="object").shape[1] == 0
         )
 
     def check_score_nan(self):
+        """score isna checking"""
         self.assertIsNotNone(train_model.best_model)
 
 
